f8e490cfb359aa61b2dd45ae8770d1eb7d1c3f8b
Sanjay Krishnan <sanjaykrishn@gmail.com>
2019-11-26 09:57:18 -0600

    Refactoring code for a 2.0 version

--- test.py
+++
@@ -1,7 +0,0 @@
-from dlstorage.utils.benchmark import *
-from dlstorage.utils.vdmsbench import *
-
-vd = VDMSStorageManager(TestTagger())
-#p2 = VDMSPerfTest(vd, 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')
-p2 = VDMSPerfTest(vd, 'f65sec.mp4')
-p2.runAll()
